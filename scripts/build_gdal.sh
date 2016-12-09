74f38d3caa14f38f41be8558913d6b2fbedd342e
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2016-12-09 14:16:32 +0100

    Update build_gdal.sh script

--- scripts/build_gdal.sh
+++ scripts/build_gdal.sh
@@ -2,12 +2,17 @@
 
 #git clone https://github.com/rouault/gdal2
 cd /home/gdal2
-git remote remove origin
-git remote add origin https://github.com/OSGeo/gdal.git
-#git checkout gmlas
+#git remote remove origin
+#git remote add origin https://github.com/OSGeo/gdal.git
 git fetch origin
-#git reset --hard origin/gmlas
-git reset --hard origin/trunk
+git checkout gmlas
+git reset --hard origin/gmlas
+#git reset --hard origin/trunk
+
+# Python bindings: gdal.VectorTranslate(): add spatSRS option
+# https://github.com/rouault/gdal2/commit/02d58a66fd3f287afb8dd5878aa1026c1e12e5c0
+git checkout 02d58a66fd3f287afb8dd5878aa1026c1e12e5c0
+
 cd gdal
 ./configure --prefix=/usr --without-libtool --with-xerces --with-curl --with-python=/usr/bin/python3 --with-spatialite
 make clean
